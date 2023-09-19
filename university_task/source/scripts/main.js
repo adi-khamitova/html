c6389cd3583f0880a4eb42bf5b5fc625eda364ff
Adelya Khamitova <adi.khamitova@gmail.com>
2023-09-20 00:13:52 +0300

    master branch and added a button with console.log

---
+++ university_task/source/scripts/main.js
@@ -0,0 +1,7 @@
+// Получаем кнопку по ее ID
+const myButton = document.getElementById("myButton");
+
+// Добавляем обработчик события для кнопки
+myButton.addEventListener("click", function() {
+    console.log("Кнопка была нажата!");
+});
